5512d5b5852aab5bc99ad181ab0063200c4c76a2
sunshifeng <18754917535@163.com>
2018-05-22 08:14:24 +0800

    new 5\22 08:14

---
+++ public/static/js/nav.js
@@ -0,0 +1,45 @@
+var navs = [{
+	"title" : "后台首页",
+	"icon" : "icon-computer",
+	"href" : "index/index/main.html",
+	"spread" : false
+},{
+	"title" : "文章列表",
+	"icon" : "icon-text",
+	"href" : "index/index/newsList.html",
+	"spread" : false
+},{
+	"title" : "友情链接",
+	"icon" : "icon-text",
+	"href" : "index/index/linksList.html",
+	"spread" : false
+},{
+	"title" : "404页面",
+	"icon" : "&#xe61c;",
+	"href" : "index/index/get404.html",
+	"spread" : false
+},{
+	"title" : "系统基本参数",
+	"icon" : "&#xe631;",
+	"href" : "index/index/systemParameter.html",
+	"spread" : false
+},{
+	"title" : "二级菜单演示",
+	"icon" : "&#xe61c;",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "二级菜单1",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "二级菜单2",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+}]
